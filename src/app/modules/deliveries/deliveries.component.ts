35cc9b92d39c0b53c368937a540734e30131f934
kaanst00 <kaj.inf@web.de>
2022-01-14 11:27:48 +0100

    Add content to deliveries

--- src/app/modules/deliveries/deliveries.component.ts
+++ src/app/modules/deliveries/deliveries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeliveriesService } from '../data/deliveries.service';
 
 @Component({
   selector: 'app-deliveries',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveriesComponent implements OnInit {
 
-  constructor() { }
+  areaDataDeliveries: any = [];
+  pieDataDeliveries: any=[];
+
+  constructor(private deliveriesService: DeliveriesService) { }
 
   ngOnInit(): void {
+    this.areaDataDeliveries= this.deliveriesService.areaDataDeliveries();
+    this.pieDataDeliveries= this.deliveriesService.pieDataDeliveries();
   }
 
 }
