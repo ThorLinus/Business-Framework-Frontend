b5b9a937700ce108425f3a8bfb68cbfba95c60e6
kaanst00 <kaj.inf@web.de>
2022-02-02 16:50:26 +0100

    Add Content to ProjectManagement

---
+++ src/app/modules/data/project-management.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectManagementService {
+
+  constructor() { }
+
+  /*
+    Hier werden die Daten für das Balkendiagramm erstellt
+    Bei fertigstellung sollen hier die Daten aus dem Backend abgerufen werden.
+  */
+  currentProjects(){
+    return[
+    {
+      name:'open',
+      data:[6,1,2,5]      
+    },
+    {
+      name:'runs',
+      data:[44, 30, 17, 10]
+    },
+    {
+      name:'done',
+      data:[20, 32,10,15]  
+    }
+
+  ]
+  }
+}
